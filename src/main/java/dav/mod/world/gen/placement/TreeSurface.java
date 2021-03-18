50bf9d4d89ea8baf4df0e947488b6371a3dc0039
Dav729 <38118091+DavD729@users.noreply.github.com>
2021-03-18 17:48:42 -0600

    Update TreeSurface.java

--- src/main/java/dav/mod/world/gen/placement/TreeSurface.java
+++ src/main/java/dav/mod/world/gen/placement/TreeSurface.java
@@ -20,14 +20,14 @@ public class TreeSurface extends BasePlacement<SurfacePlacement>{
 		int i = placementConfig.genChance;
 		boolean flag = false;
 		if(i > 0) {
-	    	for(int j = 0; j < i; ++j) {
-	    		if(random.nextInt(chance) == 0) {
-	    			int k = random.nextInt(16);
-	    			int l = random.nextInt(16);
-	    			featureIn.func_212245_a(worldIn, chunkGenerator, random, worldIn.getHeight(Heightmap.Type.MOTION_BLOCKING, pos.add(k, 0, l)), featureConfig);
-	    			flag = true;
+	    		for(int j = 0; j < i; ++j) {
+	    			if(random.nextInt(chance) == 0) {
+	    				int k = random.nextInt(16);
+	    				int l = random.nextInt(16);
+	    				featureIn.func_212245_a(worldIn, chunkGenerator, random, worldIn.getHeight(Heightmap.Type.MOTION_BLOCKING, pos.add(k, 0, l)), featureConfig);
+	    				flag = true;
+	    			}
 	    		}
-	    	}
 		}
 		return flag;
 	}
