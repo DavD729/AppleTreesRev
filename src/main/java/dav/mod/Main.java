24d022f02b125b139b943c5de29080b30837fa3d
Dav729 <38118091+DavD729@users.noreply.github.com>
2021-04-08 16:53:15 -0500

    v1.2 Update (Config & Conditions)

--- src/main/java/dav/mod/Main.java
+++ src/main/java/dav/mod/Main.java
@@ -3,6 +3,9 @@ package dav.mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import dav.mod.config.ConfigBuilder;
+import dav.mod.crafting.Condition;
+import dav.mod.crafting.ConditionSerializer;
 import dav.mod.init.BlockInit;
 import dav.mod.init.ItemInit;
 import dav.mod.world.gen.TreeWorldGen;
@@ -13,9 +16,12 @@ import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.crafting.CraftingHelper;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
@@ -26,15 +32,23 @@ import net.minecraftforge.fml.loading.FMLEnvironment;
 
 @Mod(Main.MODID)
 public class Main {
+	
     public static final String MODID = "appletreesrev";
     public static final Logger LOGGER = LogManager.getLogger();
+    public static Main instance;
 
     public Main() {
+    	instance = this;
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+        
         MinecraftForge.EVENT_BUS.register(this);
+        
+        //Config Builder Register
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ConfigBuilder.SPEC);
+        LOGGER.info("Apple Trees Rev. Mod Loaded");
     }
 
     private void setup(final FMLCommonSetupEvent event) {
@@ -42,12 +56,20 @@ public class Main {
     }
 
     private void doClientStuff(final FMLClientSetupEvent event) {
+    	//Registry Crafting Conditions
+    	CraftingHelper.register(new ConditionSerializer(new Condition(ConfigBuilder.RECIPES.GoldAppleSaplingRecipe.get(), getPath("gapple_condition")), getPath("gapplerecipecondition")));
+    	CraftingHelper.register(new ConditionSerializer(new Condition(ConfigBuilder.RECIPES.NotchAppleRecipe.get(), getPath("notch_condition")), getPath("notchrecipecondition")));
+    	LOGGER.info("Crafting Conditions Registered.");
     }
 
     private void enqueueIMC(final InterModEnqueueEvent event) {
     }
 
     private void processIMC(final InterModProcessEvent event) {
+    }
+    
+    public static ResourceLocation getPath(String path) {
+    	return new ResourceLocation(Main.MODID, path);
     }
     
     @SubscribeEvent
@@ -63,7 +85,7 @@ public class Main {
     			ItemInit.APPLE_SAPLING,
     			ItemInit.GAPPLE_SAPLING
     		);
-    		LOGGER.info("Items Registered");
+    		LOGGER.info("AppleTreesRev: Items Registered.");
         }
     	
         @SubscribeEvent
@@ -80,11 +102,7 @@ public class Main {
         		RenderTypeLookup.setRenderLayer(BlockInit.APPLE_SAPLING, RenderType.getCutout());
         		RenderTypeLookup.setRenderLayer(BlockInit.GAPPLE_SAPLING, RenderType.getCutout());
         	}
-        	LOGGER.info("Blocks Registered");
-        }
-        
-        public static ResourceLocation getPath(String path) {
-        	return new ResourceLocation(Main.MODID, path);
+        	LOGGER.info("AppleTreesRev: Blocks Registered.");
         }
     }
 }
