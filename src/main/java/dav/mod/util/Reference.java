b0d47a9fc2a4688e696d39b89dcff8c6e5d9d435
Dav729 <38118091+DavD729@users.noreply.github.com>
2021-04-09 18:18:15 -0500

    v1.6 Update

--- src/main/java/dav/mod/util/Reference.java
+++ src/main/java/dav/mod/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 	
 	public static final String MODID = "appletreesrev";
 	public static final String NAME = "Apple Trees Mod Revived";
-	public static final String VERSION = "v1.5";
+	public static final String VERSION = "v1.6";
 	public static final String CLIENT = "dav.mod.proxy.ClientProxy";
 	public static final String COMMON = "dav.mod.proxy.CommonProxy";
 }
