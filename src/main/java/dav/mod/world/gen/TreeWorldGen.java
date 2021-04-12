2efed1883e3ff3cb45384ecdb44293597d3d65f4
Dav729 <38118091+DavD729@users.noreply.github.com>
2021-04-12 17:02:13 -0500

    v1.4 World Gen Changes

--- src/main/java/dav/mod/world/gen/TreeWorldGen.java
+++ src/main/java/dav/mod/world/gen/TreeWorldGen.java
@@ -2,7 +2,7 @@ package dav.mod.world.gen;
 
 import dav.mod.world.gen.placement.SurfacePlacement;
 import dav.mod.world.gen.placement.TreeSurface;
-import dav.mod.world.gen.tree.NaturalAppleTreeFeature;
+import dav.mod.world.gen.tree.FruitTreeFeature;
 import net.minecraft.init.Biomes;
 import net.minecraft.world.gen.GenerationStage.Decoration;
 import net.minecraft.world.gen.feature.CompositeFeature;
@@ -11,18 +11,18 @@ import net.minecraft.world.gen.feature.IFeatureConfig;
 public class TreeWorldGen {
 	
 	public static void setupTreeGeneration() {
+		SurfacePlacement TreePlainsGen = new SurfacePlacement(600, 16);
+		Biomes.PLAINS.addFeature(Decoration.VEGETAL_DECORATION, new CompositeFeature<>(new FruitTreeFeature(0, true), IFeatureConfig.NO_FEATURE_CONFIG, new TreeSurface(), TreePlainsGen));
+		Biomes.SUNFLOWER_PLAINS.addFeature(Decoration.VEGETAL_DECORATION, new CompositeFeature<>(new FruitTreeFeature(0, true), IFeatureConfig.NO_FEATURE_CONFIG, new TreeSurface(), TreePlainsGen));
+		
 		SurfacePlacement TreeFlowerForest = new SurfacePlacement(70, 16);
-		Biomes.FLOWER_FOREST.addFeature(Decoration.VEGETAL_DECORATION, new CompositeFeature<>(new NaturalAppleTreeFeature(), IFeatureConfig.NO_FEATURE_CONFIG, new TreeSurface(), TreeFlowerForest));
+		Biomes.FLOWER_FOREST.addFeature(Decoration.VEGETAL_DECORATION, new CompositeFeature<>(new FruitTreeFeature(0, true), IFeatureConfig.NO_FEATURE_CONFIG, new TreeSurface(), TreeFlowerForest));
 		
-		SurfacePlacement TreeGen = new SurfacePlacement(140, 12);
-		Biomes.FOREST.addFeature(Decoration.VEGETAL_DECORATION, new CompositeFeature<>(new NaturalAppleTreeFeature(), IFeatureConfig.NO_FEATURE_CONFIG, new TreeSurface(), TreeGen));
-		Biomes.DARK_FOREST.addFeature(Decoration.VEGETAL_DECORATION, new CompositeFeature<>(new NaturalAppleTreeFeature(), IFeatureConfig.NO_FEATURE_CONFIG, new TreeSurface(), TreeGen));
+		SurfacePlacement TreeGen = new SurfacePlacement(250, 16);
+		Biomes.FOREST.addFeature(Decoration.VEGETAL_DECORATION, new CompositeFeature<>(new FruitTreeFeature(0, true), IFeatureConfig.NO_FEATURE_CONFIG, new TreeSurface(), TreeGen));
+		Biomes.DARK_FOREST.addFeature(Decoration.VEGETAL_DECORATION, new CompositeFeature<>(new FruitTreeFeature(0, true), IFeatureConfig.NO_FEATURE_CONFIG, new TreeSurface(), TreeGen));
 		
-		SurfacePlacement TreePlainsGen = new SurfacePlacement(500, 12);
-		Biomes.PLAINS.addFeature(Decoration.VEGETAL_DECORATION, new CompositeFeature<>(new NaturalAppleTreeFeature(), IFeatureConfig.NO_FEATURE_CONFIG, new TreeSurface(), TreePlainsGen));
-		Biomes.SUNFLOWER_PLAINS.addFeature(Decoration.VEGETAL_DECORATION, new CompositeFeature<>(new NaturalAppleTreeFeature(), IFeatureConfig.NO_FEATURE_CONFIG, new TreeSurface(), TreePlainsGen));
-		
-		SurfacePlacement TreeMountGen = new SurfacePlacement(150, 12);
-		Biomes.WOODED_MOUNTAINS.addFeature(Decoration.VEGETAL_DECORATION, new CompositeFeature<>(new NaturalAppleTreeFeature(), IFeatureConfig.NO_FEATURE_CONFIG, new TreeSurface(), TreeMountGen));
+		SurfacePlacement TreeMountGen = new SurfacePlacement(300, 12);
+		Biomes.WOODED_MOUNTAINS.addFeature(Decoration.VEGETAL_DECORATION, new CompositeFeature<>(new FruitTreeFeature(0, true), IFeatureConfig.NO_FEATURE_CONFIG, new TreeSurface(), TreeMountGen));
 	}
 }
