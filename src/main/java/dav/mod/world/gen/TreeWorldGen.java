b4b8b25cd5abf139c3537914f618a47363407afa
Dav729 <38118091+DavD729@users.noreply.github.com>
2021-04-16 23:01:52 -0500

    v1.4 World Gen Update

--- src/main/java/dav/mod/world/gen/TreeWorldGen.java
+++ src/main/java/dav/mod/world/gen/TreeWorldGen.java
@@ -2,28 +2,27 @@ package dav.mod.world.gen;
 
 import dav.mod.world.gen.placement.SurfacePlacement;
 import dav.mod.world.gen.placement.TreeSurface;
-import dav.mod.world.gen.tree.NaturalAppleTreeFeature;
+import dav.mod.world.gen.tree.FruitTreeFeature;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.Biomes;
 import net.minecraft.world.gen.GenerationStage.Decoration;
 import net.minecraft.world.gen.feature.IFeatureConfig;
-import net.minecraft.world.gen.feature.NoFeatureConfig;
 
 public class TreeWorldGen {
 	
 	public static void setupTreeGeneration() {
-		SurfacePlacement PlainsPlacement = new SurfacePlacement(1, 45);
-		Biomes.PLAINS.addFeature(Decoration.VEGETAL_DECORATION, Biome.createDecoratedFeature(new NaturalAppleTreeFeature(NoFeatureConfig::deserialize), IFeatureConfig.NO_FEATURE_CONFIG, new TreeSurface(SurfacePlacement::deserialize), PlainsPlacement));
-		Biomes.SUNFLOWER_PLAINS.addFeature(Decoration.VEGETAL_DECORATION, Biome.createDecoratedFeature(new NaturalAppleTreeFeature(NoFeatureConfig::deserialize), IFeatureConfig.NO_FEATURE_CONFIG, new TreeSurface(SurfacePlacement::deserialize), PlainsPlacement));
+		SurfacePlacement PlainsPlacement = new SurfacePlacement(0, 20);
+		Biomes.PLAINS.addFeature(Decoration.VEGETAL_DECORATION, Biome.createDecoratedFeature(new FruitTreeFeature(0, true), IFeatureConfig.NO_FEATURE_CONFIG, new TreeSurface(), PlainsPlacement));
+		Biomes.SUNFLOWER_PLAINS.addFeature(Decoration.VEGETAL_DECORATION, Biome.createDecoratedFeature(new FruitTreeFeature(0, true), IFeatureConfig.NO_FEATURE_CONFIG, new TreeSurface(), PlainsPlacement));
 		
-		SurfacePlacement ForestPlacement = new SurfacePlacement(1, 25);
-		Biomes.FOREST.addFeature(Decoration.VEGETAL_DECORATION, Biome.createDecoratedFeature(new NaturalAppleTreeFeature(NoFeatureConfig::deserialize), IFeatureConfig.NO_FEATURE_CONFIG, new TreeSurface(SurfacePlacement::deserialize), ForestPlacement));
-		Biomes.DARK_FOREST.addFeature(Decoration.VEGETAL_DECORATION, Biome.createDecoratedFeature(new NaturalAppleTreeFeature(NoFeatureConfig::deserialize), IFeatureConfig.NO_FEATURE_CONFIG, new TreeSurface(SurfacePlacement::deserialize), ForestPlacement));
+		SurfacePlacement ForestPlacement = new SurfacePlacement(1, 30);
+		Biomes.FOREST.addFeature(Decoration.VEGETAL_DECORATION, Biome.createDecoratedFeature(new FruitTreeFeature(0, true), IFeatureConfig.NO_FEATURE_CONFIG, new TreeSurface(), ForestPlacement));
+		Biomes.DARK_FOREST.addFeature(Decoration.VEGETAL_DECORATION, Biome.createDecoratedFeature(new FruitTreeFeature(0, true), IFeatureConfig.NO_FEATURE_CONFIG, new TreeSurface(), ForestPlacement));
 		
-		SurfacePlacement FlowerPlacement = new SurfacePlacement(3, 10);
-		Biomes.FLOWER_FOREST.addFeature(Decoration.VEGETAL_DECORATION, Biome.createDecoratedFeature(new NaturalAppleTreeFeature(NoFeatureConfig::deserialize), IFeatureConfig.NO_FEATURE_CONFIG, new TreeSurface(SurfacePlacement::deserialize), FlowerPlacement));
+		SurfacePlacement FlowerPlacement = new SurfacePlacement(3, 5);
+		Biomes.FLOWER_FOREST.addFeature(Decoration.VEGETAL_DECORATION, Biome.createDecoratedFeature(new FruitTreeFeature(0, true), IFeatureConfig.NO_FEATURE_CONFIG, new TreeSurface(), FlowerPlacement));
 		
-		SurfacePlacement MountainPlacement = new SurfacePlacement(2, 30);
-		Biomes.WOODED_MOUNTAINS.addFeature(Decoration.VEGETAL_DECORATION, Biome.createDecoratedFeature(new NaturalAppleTreeFeature(NoFeatureConfig::deserialize), IFeatureConfig.NO_FEATURE_CONFIG, new TreeSurface(SurfacePlacement::deserialize), MountainPlacement));
+		SurfacePlacement MountainPlacement = new SurfacePlacement(0, 30);
+		Biomes.WOODED_MOUNTAINS.addFeature(Decoration.VEGETAL_DECORATION, Biome.createDecoratedFeature(new FruitTreeFeature(0, true), IFeatureConfig.NO_FEATURE_CONFIG, new TreeSurface(), MountainPlacement));
 	}
 }
