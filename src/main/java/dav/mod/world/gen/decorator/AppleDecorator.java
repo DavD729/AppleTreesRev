161a9029e2ec7aba1858c277c7a9b5d3a748c0d1
Dav729 <38118091+DavD729@users.noreply.github.com>
2021-04-17 13:10:58 -0500

    v1.3 Apple Decorator Update

--- src/main/java/dav/mod/world/gen/decorator/AppleDecorator.java
+++ src/main/java/dav/mod/world/gen/decorator/AppleDecorator.java
@@ -6,6 +6,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
 import com.mojang.datafixers.Dynamic;
 import com.mojang.datafixers.types.DynamicOps;
 
@@ -46,35 +47,54 @@ public class AppleDecorator extends BeehiveTreeDecorator{
 	}
 
 	@Override
-	public void generate(IWorld world, Random rand, List<BlockPos> logPositions, List<BlockPos> leavesPositions, Set<BlockPos> Set, BlockBox Box) {
-		int i = !leavesPositions.isEmpty() ? Math.max(leavesPositions.get(0).getY() - 1, logPositions.get(0).getY()) : Math.min(logPositions.get(0).getY() + 1 + rand.nextInt(3), logPositions.get(logPositions.size() - 1).getY());
-		List<BlockPos> list = logPositions.stream().filter((BlockPos) -> {
-			return BlockPos.getY() == i;
-		}).collect(Collectors.toList());
-		if(!list.isEmpty()) {
-			BlockPos AppleLayerPos = list.get(rand.nextInt(list.size()));
+	public void generate(IWorld World, Random Rand, List<BlockPos> logPositions, List<BlockPos> leavesPositions, Set<BlockPos> Set, BlockBox Box) {
+		int i = !leavesPositions.isEmpty() ? Math.max(leavesPositions.get(0).getY() - 1, logPositions.get(0).getY()) : Math.min(logPositions.get(0).getY() + 1 + Rand.nextInt(3), logPositions.get(logPositions.size() - 1).getY());
+        List<BlockPos> list = logPositions.stream().filter((BlockPos) -> {
+           return BlockPos.getY() == i;
+        }).collect(Collectors.toList());
+        if(!list.isEmpty()) {
+        	
+        	BlockPos AppleLayerPos = list.get(Rand.nextInt(list.size()));
 			BlockState AppleType = this.getDrop();
-			int cont = 2;
-			this.setBlockStateAndEncompassPosition(world, AppleLayerPos.add(1, 0, 1), this.getNaturalAge(AppleType, rand), Set, Box);
-			this.setBlockStateAndEncompassPosition(world, AppleLayerPos.add(-1, 0, 2), this.getNaturalAge(AppleType, rand), Set, Box);
-			for(int xPos = -2; xPos < 3; xPos++) {
-				for(int zPos = -2; zPos < 3; zPos++) {
-					if(rand.nextInt(4) == 0 && cont < 8) {
-						if(isAirOrLeaves(world, AppleLayerPos.add(xPos, 0, zPos)) && isLeaves(world, AppleLayerPos.add(xPos, 1, zPos))) {
-							this.setBlockStateAndEncompassPosition(world, AppleLayerPos.add(xPos, 0, zPos), this.getNaturalAge(AppleType, rand), Set, Box);
-							cont++;
-						}
+			List<BlockPos> Spots = this.getLocations(World, AppleLayerPos);
+			
+			if(!Spots.isEmpty()) {
+				int Cont = 0;
+				
+				for(int j = 0; j < 2 && j < Spots.size(); j++) {
+					int Index = Rand.nextInt(Spots.size());
+					this.setBlockStateAndEncompassPosition(World, Spots.get(Index), this.getNaturalAge(AppleType, Rand), Set, Box);
+					Spots.remove(Index);
+					Cont++;
+				}
+				
+				for(int j = 0; j < Spots.size() && Cont < 8; j++) {
+					if(Rand.nextInt(5) == 0) {
+						this.setBlockStateAndEncompassPosition(World, Spots.get(j), this.getNaturalAge(AppleType, Rand), Set, Box);
+						Cont++;
 					}
 				}
 			}
+        }
+	}
+	
+	private List<BlockPos> getLocations(IWorld WorldIn, BlockPos Pos){
+		List<BlockPos> Locations = Lists.newArrayList();
+		for(int xPos = -2; xPos < 3; xPos++) {
+			for(int zPos = -2; zPos < 3; zPos++) {
+				if(isAirOrLeaves(WorldIn, Pos.add(xPos, 0, zPos)) && isLeaves(WorldIn, Pos.add(xPos, 1, zPos))) {
+					Locations.add(Pos.add(xPos, 0, zPos));
+				}
+			}
 		}
+		return Locations;
 	}
 	
 	private BlockState getNaturalAge(BlockState State, Random Rand) {
 		if(!this.isNatural) {
 			return State;
 		}
-		return State.with(ApplePlantBlock.AGE, Integer.valueOf(2 + Rand.nextInt(3)));
+		return State.with(ApplePlantBlock.AGE, Integer.valueOf(2 + Rand.nextInt(4)));
 	}
 	
 	protected BlockState getDrop() {
