cff5b95e126a0f6c4851e3cd98859964e3d4867d
Dav729 <38118091+DavD729@users.noreply.github.com>
2021-04-17 13:11:18 -0500

    v1.3 Small Fix

--- src/main/java/dav/mod/world/gen/decorator/TreeDecorator.java
+++ src/main/java/dav/mod/world/gen/decorator/TreeDecorator.java
@@ -1,11 +1,8 @@
 package dav.mod.world.gen.decorator;
 
 import java.util.Random;
-import java.util.function.Function;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
-
-import com.mojang.datafixers.Dynamic;
 
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.Heightmap;
@@ -16,14 +13,14 @@ import net.minecraft.world.gen.decorator.Decorator;
 
 public class TreeDecorator extends Decorator<SurfaceDecorator>{
 
-	public TreeDecorator(Function<Dynamic<?>, ? extends SurfaceDecorator> configDeserializer) {
-		super(configDeserializer);
+	public TreeDecorator() {
+		super(SurfaceDecorator::deserialize);
 	}
 
 	@Override
 	public Stream<BlockPos> getPositions(IWorld world, ChunkGenerator<? extends ChunkGeneratorConfig> generator, Random random, SurfaceDecorator config, BlockPos pos) {
 		if(random.nextInt(config.genChance) == 0) {
-			int i = random.nextInt(config.decoratorChance) + 1;
+			int i = random.nextInt(config.decoratorChance + 1) + 1;
 			return IntStream.range(0, i).mapToObj((MapObject) -> {
 				int j = random.nextInt(16);
 				int k = random.nextInt(16);
