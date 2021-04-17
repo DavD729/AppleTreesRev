a3e4b8b292bb7fd329a96f1e63cea0ee02f42aeb
Dav729 <38118091+DavD729@users.noreply.github.com>
2021-04-17 13:27:21 -0500

    v1.3 World Gen

--- src/main/java/dav/mod/world/gen/TreeWorldGen.java
+++ src/main/java/dav/mod/world/gen/TreeWorldGen.java
@@ -41,45 +41,45 @@ public class TreeWorldGen {
 	public static final ConfiguredFeature<BaseTreeFeatureConfig, ?> NATURAL = new ConfiguredFeature<BaseTreeFeatureConfig, Feature<BaseTreeFeatureConfig>>(Feature.TREE,
 		APPLE_CONFIG.func_236685_a_(ImmutableList.of(Features.Placements.BEES_005_PLACEMENT, new AppleDecorator(0, true))));
 	
-	public static final ConfiguredPlacement<SurfacePlacement> PLAINS_PLACEMENT = new ConfiguredPlacement<SurfacePlacement>(new TreeSurface(SurfacePlacement.CODEC), new SurfacePlacement(40));
-	public static final ConfiguredPlacement<SurfacePlacement> SUN_PLAINS_PLACEMENT = new ConfiguredPlacement<SurfacePlacement>(new TreeSurface(SurfacePlacement.CODEC), new SurfacePlacement(35));
-	public static final ConfiguredPlacement<SurfacePlacement> FOREST_PLACEMENT = new ConfiguredPlacement<SurfacePlacement>(new TreeSurface(SurfacePlacement.CODEC), new SurfacePlacement(25));
-	public static final ConfiguredPlacement<SurfacePlacement> DARK_FOREST_PLACEMENT = new ConfiguredPlacement<SurfacePlacement>(new TreeSurface(SurfacePlacement.CODEC), new SurfacePlacement(20));
-	public static final ConfiguredPlacement<SurfacePlacement> FLOWER_FOREST_PLACEMENT = new ConfiguredPlacement<SurfacePlacement>(new TreeSurface(SurfacePlacement.CODEC), new SurfacePlacement(8));
-	public static final ConfiguredPlacement<SurfacePlacement> MOUNTAIN_PLACEMENT = new ConfiguredPlacement<SurfacePlacement>(new TreeSurface(SurfacePlacement.CODEC), new SurfacePlacement(60));
+	public static final ConfiguredPlacement<SurfacePlacement> PLAINS_PLACEMENT = new ConfiguredPlacement<SurfacePlacement>(new TreeSurface(SurfacePlacement.CODEC), new SurfacePlacement(20));
+	public static final ConfiguredPlacement<SurfacePlacement> SUN_PLAINS_PLACEMENT = new ConfiguredPlacement<SurfacePlacement>(new TreeSurface(SurfacePlacement.CODEC), new SurfacePlacement(20));
+	public static final ConfiguredPlacement<SurfacePlacement> FOREST_PLACEMENT = new ConfiguredPlacement<SurfacePlacement>(new TreeSurface(SurfacePlacement.CODEC), new SurfacePlacement(30));
+	public static final ConfiguredPlacement<SurfacePlacement> DARK_FOREST_PLACEMENT = new ConfiguredPlacement<SurfacePlacement>(new TreeSurface(SurfacePlacement.CODEC), new SurfacePlacement(30));
+	public static final ConfiguredPlacement<SurfacePlacement> FLOWER_FOREST_PLACEMENT = new ConfiguredPlacement<SurfacePlacement>(new TreeSurface(SurfacePlacement.CODEC), new SurfacePlacement(5));
+	public static final ConfiguredPlacement<SurfacePlacement> MOUNTAIN_PLACEMENT = new ConfiguredPlacement<SurfacePlacement>(new TreeSurface(SurfacePlacement.CODEC), new SurfacePlacement(30));
 	
 	@SubscribeEvent(priority = EventPriority.HIGH)
 	public static void setupTreeGeneration(final BiomeLoadingEvent event) {
 		BiomeGenerationSettingsBuilder generation = event.getGeneration();
-		
+		String Evt = event.getName().toString();
 		switch (event.getCategory()) {
 
-		case PLAINS:		if(event.getName().toString().contains(Biomes.PLAINS.getLocation().toString())) {
-	    					registerTo(generation, NATURAL.withPlacement(PLAINS_PLACEMENT));
-	    					Main.LOGGER.info("PLAINS BIOME Feature Subscribed");
-	    				} else if(event.getName().toString().contains(Biomes.SUNFLOWER_PLAINS.getLocation().toString())) {
-	    					registerTo(generation, NATURAL.withPlacement(SUN_PLAINS_PLACEMENT));
-	    					Main.LOGGER.info("SUNFLOWER_PLAINS BIOME Feature Subscribed");
-	    				}
-	    				break;
-	    			 
-		case FOREST:		if(event.getName().toString().contains(Biomes.FOREST.getLocation().toString())) {
-	    					registerTo(generation, NATURAL.withPlacement(FOREST_PLACEMENT));
-	    					Main.LOGGER.info("FOREST BIOME Feature Subscribed");
-	    				} else if(event.getName().toString().contains(Biomes.DARK_FOREST.getLocation().toString())) {
-	    					registerTo(generation, NATURAL.withPlacement(DARK_FOREST_PLACEMENT));
-	    					Main.LOGGER.info("DARK FOREST BIOME Feature Subscribed");
-	    				} else if(event.getName().toString().contains(Biomes.FLOWER_FOREST.getLocation().toString())) {
-	    					registerTo(generation, NATURAL.withPlacement(FLOWER_FOREST_PLACEMENT));
-	    					Main.LOGGER.info("FLOWER FOREST BIOME Feature Subscribed");
-	    				}
-	    				break;
-		 			 
-		case EXTREME_HILLS:	if(event.getName().toString().contains(Biomes.WOODED_MOUNTAINS.getLocation().toString())) {
-						registerTo(generation, NATURAL.withPlacement(MOUNTAIN_PLACEMENT));
-						Main.LOGGER.info("WOODED MOUNTAINS BIOME Feature Subscribed");
-	    				}
-		default: 	break;
+		case PLAINS:	if(Evt.equals(Biomes.PLAINS.getLocation().toString())) {
+							registerTo(generation, NATURAL.withPlacement(PLAINS_PLACEMENT));
+							Main.LOGGER.info("PLAINS Feature Subscribed");
+						} else if(Evt.equals(Biomes.SUNFLOWER_PLAINS.getLocation().toString())) {
+							registerTo(generation, NATURAL.withPlacement(SUN_PLAINS_PLACEMENT));
+							Main.LOGGER.info("SUNFLOWER_PLAINS Feature Subscribed");
+						}
+						break;
+
+		case FOREST:	if(Evt.equals(Biomes.FOREST.getLocation().toString())) {
+							registerTo(generation, NATURAL.withPlacement(FOREST_PLACEMENT));
+							Main.LOGGER.info("FOREST Feature Subscribed");
+						} else if(Evt.equals(Biomes.DARK_FOREST.getLocation().toString())) {
+							registerTo(generation, NATURAL.withPlacement(DARK_FOREST_PLACEMENT));
+							Main.LOGGER.info("DARK FOREST Feature Subscribed");
+						} else if(Evt.equals(Biomes.FLOWER_FOREST.getLocation().toString())) {
+							registerTo(generation, NATURAL.withPlacement(FLOWER_FOREST_PLACEMENT));
+							Main.LOGGER.info("FLOWER FOREST Feature Subscribed");
+						}
+						break;
+
+		case EXTREME_HILLS:		if(Evt.equals(Biomes.WOODED_MOUNTAINS.getLocation().toString())) {
+									registerTo(generation, NATURAL.withPlacement(MOUNTAIN_PLACEMENT));
+									Main.LOGGER.info("WOODED MOUNTAINS Feature Subscribed");
+								}
+		default:	break;
 		}
 	}
 	
