653045bb0646be88ebbeb2652a63d0d00acdd73d
Dav729 <38118091+DavD729@users.noreply.github.com>
2021-04-16 23:01:11 -0500

    v1.4 Small Change

--- src/main/java/dav/mod/world/gen/placement/TreeSurface.java
+++ src/main/java/dav/mod/world/gen/placement/TreeSurface.java
@@ -1,11 +1,8 @@
 package dav.mod.world.gen.placement;
 
 import java.util.Random;
-import java.util.function.Function;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
-
-import com.mojang.datafixers.Dynamic;
 
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IWorld;
@@ -16,14 +13,14 @@ import net.minecraft.world.gen.placement.Placement;
 
 public class TreeSurface extends Placement<SurfacePlacement>{
 
-	public TreeSurface(Function<Dynamic<?>, ? extends SurfacePlacement> configFactoryIn) {
-		super(configFactoryIn);
+	public TreeSurface() {
+		super(SurfacePlacement::deserialize);
 	}
 
 	@Override
 	public Stream<BlockPos> getPositions(IWorld worldIn, ChunkGenerator<? extends GenerationSettings> generatorIn, Random random, SurfacePlacement configIn, BlockPos pos) {
 		if(random.nextInt(configIn.genChance) == 0) {
-			int i = random.nextInt(configIn.surfaceChance) + 1;
+			int i = random.nextInt(configIn.surfaceChance + 1) + 1;
 			return IntStream.range(0, i).mapToObj((MapObject) -> {
 				int j = random.nextInt(16);
 				int k = random.nextInt(16);
