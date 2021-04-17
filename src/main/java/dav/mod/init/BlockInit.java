72cf2aa3a8812d1439b5937c3875a5afa96888cc
Dav729 <38118091+DavD729@users.noreply.github.com>
2021-04-17 13:13:37 -0500

    v1.3 Changes

--- src/main/java/dav/mod/init/BlockInit.java
+++ src/main/java/dav/mod/init/BlockInit.java
@@ -4,8 +4,10 @@ import dav.mod.objects.blocks.tree.ApplePlantBlock;
 import dav.mod.objects.blocks.tree.CustomSaplingBlock;
 import dav.mod.objects.blocks.tree.CustomTree;
 import dav.mod.world.gen.TreeWorldGen;
+import net.fabricmc.api.EnvType;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.loader.launch.common.FabricLauncherBase;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.client.render.RenderLayer;
@@ -21,10 +23,12 @@ public class BlockInit {
 	public static final Block APPLE_SAPLING = new CustomSaplingBlock(new CustomTree(Feature.NORMAL_TREE.configure(TreeWorldGen.APPLE_TREE_CONFIG)), FabricBlockSettings.of(Material.PLANT).sounds(BlockSoundGroup.GRASS).ticksRandomly());
 	public static final Block GAPPLE_SAPLING = new CustomSaplingBlock(new CustomTree(Feature.NORMAL_TREE.configure(TreeWorldGen.GOLD_APPLE_TREE_CONFIG)), FabricBlockSettings.of(Material.PLANT).sounds(BlockSoundGroup.GRASS).ticksRandomly());
 	
-	public static void renderCutoutBlocks() {
-		BlockRenderLayerMap.INSTANCE.putBlock(APPLE_SAPLING, RenderLayer.getCutout());
-		BlockRenderLayerMap.INSTANCE.putBlock(GAPPLE_SAPLING, RenderLayer.getCutout());
-		BlockRenderLayerMap.INSTANCE.putBlock(APPLE_PLANT, RenderLayer.getCutout());
-		BlockRenderLayerMap.INSTANCE.putBlock(GAPPLE_PLANT, RenderLayer.getCutout());
+	public static void renderBlocks() {
+		if(FabricLauncherBase.getLauncher().getEnvironmentType() == EnvType.CLIENT) {
+			BlockRenderLayerMap.INSTANCE.putBlock(APPLE_SAPLING, RenderLayer.getCutout());
+			BlockRenderLayerMap.INSTANCE.putBlock(GAPPLE_SAPLING, RenderLayer.getCutout());
+			BlockRenderLayerMap.INSTANCE.putBlock(APPLE_PLANT, RenderLayer.getCutout());
+			BlockRenderLayerMap.INSTANCE.putBlock(GAPPLE_PLANT, RenderLayer.getCutout());
+		}
 	}
 }
