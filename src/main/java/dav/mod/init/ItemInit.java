02fba4bbc74d039a7a8dbd8a3592fffbe61a18be
Dav729 <38118091+DavD729@users.noreply.github.com>
2021-04-17 13:30:45 -0500

    v1.3 Changes

--- src/main/java/dav/mod/init/ItemInit.java
+++ src/main/java/dav/mod/init/ItemInit.java
@@ -8,4 +8,7 @@ public class ItemInit {
 	
 	public static Item APPLE_SAPLING = new BlockItem(BlockInit.APPLE_SAPLING, new Item.Properties().group(ItemGroup.DECORATIONS)).setRegistryName(BlockInit.APPLE_SAPLING.getRegistryName());
 	public static Item GAPPLE_SAPLING = new BlockItem(BlockInit.GAPPLE_SAPLING, new Item.Properties().group(ItemGroup.DECORATIONS)).setRegistryName(BlockInit.GAPPLE_SAPLING.getRegistryName());
+	
+	public static Item APPLE_PLANT = new BlockItem(BlockInit.APPLE_PLANT, new Item.Properties()).setRegistryName(BlockInit.APPLE_PLANT.getRegistryName());
+	public static Item GAPPLE_PLANT = new BlockItem(BlockInit.GAPPLE_PLANT, new Item.Properties()).setRegistryName(BlockInit.GAPPLE_PLANT.getRegistryName());
 }
