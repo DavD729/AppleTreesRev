e4fd7622efdc4707e09f1ac9b6925effa6eee164
Dav729 <38118091+DavD729@users.noreply.github.com>
2021-04-17 13:42:17 -0500

    v1.3 Mixin Update

--- src/main/java/dav/mod/mixin/RecipeManagerMixin.java
+++ src/main/java/dav/mod/mixin/RecipeManagerMixin.java
@@ -20,11 +20,14 @@ import net.minecraft.util.profiler.Profiler;
 public class RecipeManagerMixin {
 	@Inject(method = "apply", at = @At("HEAD"))
 	private void interceptApply(Map<Identifier, JsonElement> map, ResourceManager resourceManager, Profiler profiler, CallbackInfo info) {
+		System.out.println("Apple Trees Rev.: Mixin Fired");
 		if(RecipesBuilder.GAPPLE_SAPLING_RECIPE != null && ConfigBuilder.craftGoldAppleSapling) {
 			map.put(new Identifier("appletreesrev", "gapple_sapling"), RecipesBuilder.GAPPLE_SAPLING_RECIPE);
+			System.out.println("Apple Trees Rev.: Gold Apple Sapling Recipe Added");
 		}
 		if(RecipesBuilder.NOTCH_APPLE_RECIPE != null && ConfigBuilder.craftNotchApple) {
 			map.put(new Identifier("enchanted_golden_apple"), RecipesBuilder.NOTCH_APPLE_RECIPE);
+			System.out.println("Apple Trees Rev.: Notch Apple Recipe Added");
 		}
 	}
 }
