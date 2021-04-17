bdac51ad3d2408b48256832a5cc17f79bbf32ece
Dav729 <38118091+DavD729@users.noreply.github.com>
2021-04-17 13:30:11 -0500

    v1.3 Update (World Gen & Server Fix)

--- src/main/java/dav/mod/Main.java
+++ src/main/java/dav/mod/Main.java
@@ -10,11 +10,8 @@ import dav.mod.init.BlockInit;
 import dav.mod.init.ItemInit;
 import dav.mod.world.gen.TreeWorldGen;
 import net.minecraft.block.Block;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.crafting.CraftingHelper;
 import net.minecraftforge.event.RegistryEvent;
@@ -22,87 +19,69 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fml.loading.FMLEnvironment;
 
 @Mod(Main.MODID)
 public class Main {
 	
-    public static final String MODID = "appletreesrev";
-    public static final Logger LOGGER = LogManager.getLogger();
-    public static Main instance;
+	public static final String MODID = "appletreesrev";
+	public static final Logger LOGGER = LogManager.getLogger();
+	public static Main instance;
 
-    public Main() {
-    	instance = this;
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
-        
-        MinecraftForge.EVENT_BUS.register(this);
-        
-        //Config Builder Register
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ConfigBuilder.SPEC);
-        LOGGER.info("Apple Trees Rev. Mod Loaded");
-    }
+	public Main() {
+		
+		instance = this;
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
+		MinecraftForge.EVENT_BUS.register(this);
+		
+		//Config Builder Register
+		ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ConfigBuilder.SPEC);
+		LOGGER.info("Apple Trees Rev. Mod Loaded");
+	}
 
-    private void setup(final FMLCommonSetupEvent event) {
-    	MinecraftForge.EVENT_BUS.register(TreeWorldGen.class);
-    }
-
-    private void doClientStuff(final FMLClientSetupEvent event) {
-    	//Registry Crafting Conditions
-    	CraftingHelper.register(new ConditionSerializer(new Condition(ConfigBuilder.RECIPES.GoldAppleSaplingRecipe.get(), getPath("gapple_condition")), getPath("gapplerecipecondition")));
-    	CraftingHelper.register(new ConditionSerializer(new Condition(ConfigBuilder.RECIPES.NotchAppleRecipe.get(), getPath("notch_condition")), getPath("notchrecipecondition")));
-    	LOGGER.info("Crafting Conditions Registered.");
-    }
-
-    private void enqueueIMC(final InterModEnqueueEvent event) {
-    }
-
-    private void processIMC(final InterModProcessEvent event) {
-    }
-    
-    public static ResourceLocation getPath(String path) {
-    	return new ResourceLocation(Main.MODID, path);
-    }
-    
-    @SubscribeEvent
-    public void onServerStarting(FMLServerStartingEvent event) {
-    }
-    
-    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
-    	
-    	@SubscribeEvent
-        public static void onItemsRegistry(final RegistryEvent.Register<Item> itemRegistryEvent) {
-    		itemRegistryEvent.getRegistry().registerAll(
-    			ItemInit.APPLE_SAPLING,
-    			ItemInit.GAPPLE_SAPLING
-    		);
-    		LOGGER.info("AppleTreesRev: Items Registered.");
-        }
-    	
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-        	blockRegistryEvent.getRegistry().registerAll(
-        		BlockInit.APPLE_PLANT,
-        		BlockInit.GAPPLE_PLANT,
-        		BlockInit.APPLE_SAPLING,
-        		BlockInit.GAPPLE_SAPLING
-        	);
-        	if(FMLEnvironment.dist == Dist.CLIENT) {
-        		RenderTypeLookup.setRenderLayer(BlockInit.APPLE_PLANT, RenderType.getCutout());
-        		RenderTypeLookup.setRenderLayer(BlockInit.GAPPLE_PLANT, RenderType.getCutout());
-        		RenderTypeLookup.setRenderLayer(BlockInit.APPLE_SAPLING, RenderType.getCutout());
-        		RenderTypeLookup.setRenderLayer(BlockInit.GAPPLE_SAPLING, RenderType.getCutout());
-        	}
-        	LOGGER.info("AppleTreesRev: Blocks Registered.");
-        }
-    }
+	private void commonSetup(final FMLCommonSetupEvent event) {
+		MinecraftForge.EVENT_BUS.register(TreeWorldGen.class);
+		LOGGER.info("Common: World Gen Class Registered.");
+		
+		CraftingHelper.register(new ConditionSerializer(new Condition(ConfigBuilder.RECIPES.GoldAppleSaplingRecipe.get(), getPath("gapplerecipecondition"))));
+		CraftingHelper.register(new ConditionSerializer(new Condition(ConfigBuilder.RECIPES.NotchAppleRecipe.get(), getPath("notchrecipecondition"))));
+		LOGGER.info("Common: Crafting Conditions Registered.");
+	}
+	
+	public static ResourceLocation getPath(String path) {
+		return new ResourceLocation(Main.MODID, path);
+	}
+	
+	@SubscribeEvent
+	public void onServerStarting(FMLServerStartingEvent event) {
+	}
+	
+	@Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
+	public static class RegistryEvents {
+		
+		@SubscribeEvent
+		public static void onItemsRegistry(final RegistryEvent.Register<Item> itemRegistryEvent) {
+			itemRegistryEvent.getRegistry().registerAll(
+				ItemInit.APPLE_SAPLING,
+				ItemInit.GAPPLE_SAPLING,
+				ItemInit.APPLE_PLANT,
+				ItemInit.GAPPLE_PLANT
+			);
+			LOGGER.info("AppleTreesRev: Items Registered.");
+		}
+		
+		@SubscribeEvent
+		public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
+			blockRegistryEvent.getRegistry().registerAll(
+				BlockInit.APPLE_PLANT,
+				BlockInit.GAPPLE_PLANT,
+				BlockInit.APPLE_SAPLING,
+				BlockInit.GAPPLE_SAPLING
+			);
+			BlockInit.onBlockRendering();
+			LOGGER.info("AppleTreesRev: Blocks Registered.");
+		}
+	}
 }
