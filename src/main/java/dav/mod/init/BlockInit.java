006572838914a2f44b3db254fd84090de00b81cd
Dav729 <38118091+DavD729@users.noreply.github.com>
2021-04-17 13:29:13 -0500

    v1.3 Changes

--- src/main/java/dav/mod/init/BlockInit.java
+++ src/main/java/dav/mod/init/BlockInit.java
@@ -7,12 +7,25 @@ import dav.mod.objects.blocks.tree.CustomTree;
 import dav.mod.world.gen.TreeWorldGen;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 
 public class BlockInit {
 	
-	public static Block APPLE_PLANT = new ApplePlantBlock(Block.Properties.create(Material.PLANTS), 0).setRegistryName(Main.RegistryEvents.getPath("apple_plant"));
-	public static Block GAPPLE_PLANT = new ApplePlantBlock(Block.Properties.create(Material.PLANTS), 1).setRegistryName(Main.RegistryEvents.getPath("gapple_plant"));
+	public static Block APPLE_PLANT = new ApplePlantBlock(Block.Properties.create(Material.PLANTS), 0).setRegistryName(Main.getPath("apple_plant"));
+	public static Block GAPPLE_PLANT = new ApplePlantBlock(Block.Properties.create(Material.PLANTS), 1).setRegistryName(Main.getPath("gapple_plant"));
 	
-	public static Block APPLE_SAPLING = new AppleSaplingBlock(new CustomTree(TreeWorldGen.APPLE), Block.Properties.create(Material.PLANTS)).setRegistryName(Main.RegistryEvents.getPath("apple_sapling"));
-	public static Block GAPPLE_SAPLING = new AppleSaplingBlock(new CustomTree(TreeWorldGen.GAPPLE), Block.Properties.create(Material.PLANTS)).setRegistryName(Main.RegistryEvents.getPath("gapple_sapling"));
+	public static Block APPLE_SAPLING = new AppleSaplingBlock(new CustomTree(TreeWorldGen.APPLE), Block.Properties.create(Material.PLANTS)).setRegistryName(Main.getPath("apple_sapling"));
+	public static Block GAPPLE_SAPLING = new AppleSaplingBlock(new CustomTree(TreeWorldGen.GAPPLE), Block.Properties.create(Material.PLANTS)).setRegistryName(Main.getPath("gapple_sapling"));
+	
+	public static void onBlockRendering() {
+		if(FMLEnvironment.dist == Dist.CLIENT) {
+    		RenderTypeLookup.setRenderLayer(BlockInit.APPLE_PLANT, RenderType.getCutout());
+    		RenderTypeLookup.setRenderLayer(BlockInit.GAPPLE_PLANT, RenderType.getCutout());
+    		RenderTypeLookup.setRenderLayer(BlockInit.APPLE_SAPLING, RenderType.getCutout());
+    		RenderTypeLookup.setRenderLayer(BlockInit.GAPPLE_SAPLING, RenderType.getCutout());
+    	}
+	}
 }
